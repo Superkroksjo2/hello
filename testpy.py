b69075f2dca300c54b386eea304aee0591100a57
Superkroksjo2 <90190522+Superkroksjo2@users.noreply.github.com>
2021-09-13 13:13:55 +0200

    ✨ MaTh Update

--- testpy.py
+++ testpy.py
@@ -6,3 +6,23 @@ print(f"hej på dig {name}")
 
 age = input("hur gammal är du")
 print(f"oj så gammal du år hella {age} år.")
+
+tal1 = 12
+
+tal2 = float(input("skriv ett tal:"))
+
+summa = tal1 * tal2
+
+print(f"summan blir {summa}")
+
+summa = tal1 % tal2
+
+print(f"summan blir {summa}")
+
+summa = tal1 - tal2
+
+print(f"summan blir {summa}")
+
+summa = tal1 + tal2
+
+print(f"summan blir {summa}")
